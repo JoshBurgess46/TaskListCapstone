a7a89d9f601fbff663407c49ff99c4729138acba
Josh Burgess <JoshBurgess_46@outlook.com>
2019-08-26 08:48:53 -0400

    Task List Capstone

---
+++ TaskListCapstone/Controllers/TaskListController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using TaskListCapstone.Models;
+
+namespace TaskListCapstone.Controllers
+{
+    [Authorize]
+    public class TaskListController : Controller
+    {
+        private readonly TaskListDbContext _context;
+        public TaskListController(TaskListDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult TaskList()
+        {
+            string id = User.FindFirst(ClaimTypes.NameIdentifier).Value;
+            List<TaskList> databaseList = _context.TaskList.ToList();
+            
+            List<TaskList> newTask = new List<TaskList>();
+            AspNetUsers thisUser = _context.AspNetUsers.Where(u => u.UserName == User.Identity.Name).First();
+
+            foreach (var item in databaseList)
+            {
+                if (id==item.Id)
+                {
+
+                }
+            }
+            return View(newTask);
+        }
+        public IActionResult AddTask()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddTask(TaskList newTask)
+        {
+            AspNetUsers thisUser = _context.AspNetUsers.Where(u => u.UserName == User.Identity.Name).First();
+            newTask.Description = thisUser.Id;
+            _context.TaskList.Add(newTask);
+            _context.SaveChanges();
+            return RedirectToAction("TaskList");
+        }
+    }
+}
---
+++ TaskListCapstone/Models/TaskList.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TaskListCapstone.Models
+{
+    public partial class TaskList
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public DateTime DueDate { get; set; }
+        public string Complete { get; set; }
+    }
+}
